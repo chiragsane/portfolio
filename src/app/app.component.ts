237783bfa54925290086c26c35337bf632111e06
Chirag Sane <chiragsane333@gmail.com>
2017-11-18 20:46:36 +0530

    working semantic modal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'pf-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'pf';
+export class AppComponent implements OnInit {
+  constructor() { }
+  ngOnInit() {
+  }
+  openModal = () => $('.ui.basic.modal').modal('show');
 }
